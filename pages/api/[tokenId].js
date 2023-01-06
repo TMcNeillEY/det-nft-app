91b4b46939b5610ba04fe50ddb0c8637501a3e30
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-06 01:38:53 -0500

    updating images

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -3,14 +3,15 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://github.com/TMcNeillEY/det-nft-app/tree/main/public/cryptodevs";
+   `https://raw.githubusercontent.com/TMcNeillEY/det-nft-app/a5beaecddbc0d48efdedfe16a2bca7fb9a480ce5/public/cryptodevs/${Number(tokenId)-1}.svg`
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
     // More info can be found here: https://docs.opensea.io/docs/metadata-standards
-    res.status(200).json({
+    return res.status(200).json({
       name: "DET Dev #" + tokenId,
       description: "DET Dev is a collection of developers in crypto",
-      image: image_url + tokenId + ".svg",
+      image: image_url,
+    //   image: image_url + tokenId + ".svg",
     });
   }
